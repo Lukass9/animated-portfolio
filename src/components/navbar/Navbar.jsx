d8f61b4dc4ee4566d1ef0ab7498b7db9626c7910
Lukass9 <l_pisarek@o2.pl>
2023-10-20 08:28:48 +0200

    add animation to hero section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
 					transition={{ duration: 0.5 }}>
-					Najemaly
+					LukeCodeSribe
 				</motion.span>
 				<div className='social'>
 					<a href='#'>
